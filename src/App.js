f0971da5360abca5dadfef9030b51b864e7071ab
AmitRai670 <8387amit@gmail.com>
2025-02-18 13:49:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import Header from "./component.js/Header";
+
+function App() {
+  return (
+   <div>
+   <Header></Header>
+ 
+   </div>
+  );
+}
+
+export default App;
---
+++ src/component.js/Header.js
@@ -0,0 +1,12 @@
+import '../component.js/Header.css';
+
+function Header()
+{
+    return(
+        <div>
+            <h1>AMIT</h1>
+            <p className='para'>RAI</p>
+        </div>
+    );
+}
+export default Header;
